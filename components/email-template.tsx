f9c7b84f5d7bd4f42aa174cd5581070598c82cf4
rikiTada <riki.tada777@gmail.com>
2024-05-31 14:13:06 +0900

    feat: add email-template.

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -11,6 +11,9 @@ import {
   Container,
   Section,
   Img,
+  Column,
+  Row,
+  Hr,
 } from "@react-email/components";
 import * as React from "react";
 
@@ -23,7 +26,7 @@ export const EmailTemplate: React.FC<Readonly<FormData>> = ({
   email,
   body,
 }) => {
-  const previewText = `Join ${name} on Vercel`;
+  const previewText = `${name}さん お問い合わせを受け付けました。`;
 
   return (
     <Html>
@@ -42,13 +45,60 @@ export const EmailTemplate: React.FC<Readonly<FormData>> = ({
                 className="my-0 mx-auto"
               /> */}
             </Section>
-            <Heading>ようこそ, {name}さん！</Heading>
+            <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
+              <strong>{name}</strong>さん、お問い合わせが<strong>完了</strong>
+              しました。
+            </Heading>
 
-            <Text>お問い合わせありがとうございます！</Text>
-            <Text>名前:{name}</Text>
-            <Text>メールアドレス:{email}</Text>
+            <Text></Text>
+            <Text className="text-black text-[14px] leading-[24px]">
+              {name}さん。この度は、お問い合わせありがとうございます！
+            </Text>
+            <Text>メールアドレス: {email}</Text>
 
-            <Text>{body}</Text>
+            <Section>
+              <Row>
+                <Column align="right">
+                  <Img
+                    className="rounded-full"
+                    src={`${baseUrl}/icon/admin.png`}
+                    width="64"
+                    height="64"
+                  />
+                </Column>
+                <Column align="center">
+                  <Img
+                    src={`${baseUrl}/icon/vercel-arrow.png`}
+                    width="12"
+                    height="9"
+                    alt="invited you to"
+                  />
+                </Column>
+                <Column align="left">
+                  <Img
+                    className="rounded-full"
+                    src={`${baseUrl}/icon/animal_hiyoko.png`}
+                    width="64"
+                    height="64"
+                  />
+                </Column>
+              </Row>
+            </Section>
+
+            <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
+
+            <Text className="text-lg text-[#484848] p-6 bg-[#f2f3f3] rounded">
+              {body}
+            </Text>
+
+            <Section className="text-center mt-[32px] mb-[32px]">
+              <Button
+                className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
+                href={`${baseUrl}`}
+              >
+                トップページに戻る
+              </Button>
+            </Section>
           </Container>
         </Body>
       </Tailwind>
