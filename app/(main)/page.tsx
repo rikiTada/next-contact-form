c3b12a94b41bbea3dd2d8e004cf7c78daae28498
rikiTada <riki.tada777@gmail.com>
2024-06-02 14:52:54 +0900

    fix: layout分離のためフォルダ移動

---
+++ app/(main)/page.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default async function Home() {
+  return <Link href="/contact">⇒ Contact</Link>;
+}
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import ContactForm from "@/components/contact-form";
-
-export default async function Home() {
-  return <ContactForm />;
-}
